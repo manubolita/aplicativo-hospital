125a923df8fd1d30ad21b7c9abf48e12d6e8e579
Juanjher101 <147109086+Juanjher101@users.noreply.github.com>
2024-01-16 13:14:03 -0500

    Add files via upload

--- farmacia.py
+++ farmacia.py
@@ -1,4 +1,5 @@
 import pickle
+import os
 class FARMACO:
     def __init__(self,nombre,id_producto,valor,cantidad):
         self.nombre=nombre
@@ -24,25 +25,25 @@ class FARMACIA:
     def registrar_producto(self):
         while True:
             nombre= input("digite el nombre del producto ---> ")
-            id_producto = self.id_producto
-            self.id_producto+=1
-            valor= float(input("Ingrese valor del producto ---> "))
-            cantidad=int(input("Cuantas unidades tiene? ---> "))
-            producto=FARMACO(nombre,id_producto,valor,cantidad)
-            print(f"Producto ---> {producto}")
-            if self.buscar_producto(producto)!=False:
-                print("Id en uso, imposible agregar producto... ")
+            if self.buscar_producto(nombre)!=False:
+                print("El producto ya existe, imposible agregar producto... ")
             else:
+                id_producto = self.id_producto
+                self.id_producto+=1
+                valor= float(input("Ingrese valor del producto ---> "))
+                cantidad=int(input("Cuantas unidades tiene? ---> "))
+                producto=FARMACO(nombre,id_producto,valor,cantidad)
+                print(f"Producto ---> {producto}")
                 self.adicionar_producto(producto)
                 self.guardar_productos()
                 print("producto ingresado correctamente")
             salir = input('Presione cualquier tecla para salir, o ingrese 1 para continuar: ')
             if salir != '1':
                 break
-    def buscar_producto(self,producto):
+    def buscar_producto(self,nombre):
         for p in self.productos:
-            if p.id_producto== producto.id_producto:
-                return producto
+            if p.nombre == nombre:
+                return p
         else:
             return False
     
@@ -59,14 +60,15 @@ class FARMACIA:
     
     def pedir_producto(self):
         nombre= input("Ingrese el nombre del producto que desea pedir: ")
-        for producto in self.productos:
-            if producto.nombre.lower()== nombre.lower():
-                cantidad_pedir= int(input("Ingrese la cantidad que desea pedir: "))
-                if cantidad_pedir>0:
-                    producto.cantidad+= cantidad_pedir
-                    print (f"Se han agrgado {cantidad_pedir} unidades del producto {nombre} al inventario.")
-                else:
-                    print("La cantidad debe ser mayor a cero. ")
-                break
+        if self.buscar_producto(nombre)== False:
+            print(f'No se ha encontrado un producto con el nombre: {nombre}')
+        else:
+            producto = self.buscar_producto(nombre)
+            cantidad_pedir= int(input("Ingrese la cantidad que desea pedir: "))
+            if cantidad_pedir>0:
+                producto.cantidad+= cantidad_pedir
+                print (f"Se han agrgado {cantidad_pedir} unidades del producto {nombre} al inventario.")
             else:
-                print(f"No se encontro ningun producto con el nombre {nombre}")
+                print("La cantidad debe ser mayor a cero. ")
+        os.system('pause')
+        
